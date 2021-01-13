1e40a987d8ec5f44b67f51edb04f9eb5b5573b6b
NicolasDufeu <nicoco-78@hotmail.fr>
2021-01-13 13:25:59 +0100

    Premiere sauvegarde

---
+++ src/app/folder/folder.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-folder',
+  templateUrl: './folder.page.html',
+  styleUrls: ['./folder.page.scss'],
+})
+export class FolderPage implements OnInit {
+    
+  //public folder: string;
+  folder = "";
+  name = "";
+
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    //this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+      
+    this.activatedRoute.params.subscribe( (params) =>{
+        
+        console.log('params', params);
+        
+        this.folder = params.id;
+        this.name = params.name;
+        
+    })
+  }
+
+}
